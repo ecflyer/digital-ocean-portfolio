b918f8282fe984034f14ea0e80bc4efabe48c8cf
Eric Corbett <ericcorbett@Erics-iMac.home>
2022-09-02 14:10:32 -0700

    Update color scheme - stage 01

--- src/components/profile/Work.js
+++ src/components/profile/Work.js
@@ -1,14 +1,5 @@
 import React from 'react'
 let myWorkData = require('../portfolio/workData.json')
-// import img02 from '../../images/portfolio-02.jpg'
-// import img03 from '../../images/portfolio-03.jpg'
-// import img04 from '../../images/portfolio-04.jpg'
-// import img05 from '../../images/portfolio-05.jpg'
-// import img06 from '../../images/portfolio-06.jpg'
-// import img07 from '../../images/portfolio-07.jpg'
-// import img08 from '../../images/portfolio-08.jpg'
-// import img09 from '../../images/portfolio-09.jpg'
-// import img10 from '../../images/portfolio-10.jpg'
 
 export default function Work() {
     const workData = myWorkData.map(work => (
